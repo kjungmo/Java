00e6c63f5f8ba043cd68e2ce5d42b74c01e25464
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-24 00:25:19 +0900

    깊은 복제 후 복제본 변경은 원본에 영향을 미치지 않는다 실행 클래스

--- chap11/src/sec03/exam05_deep_clone/MemberExample.java
+++ chap11/src/sec03/exam05_deep_clone/MemberExample.java
@@ -14,7 +14,7 @@ public class MemberExample {
 		System.out.println("[복제 객체의 필드값]");
 		System.out.println("name: " + cloned.name);
 		System.out.println("age: " + cloned.age);
-		System.out.println("scores: {");
+		System.out.print("scores: {");
 		for(int i = 0; i<cloned.scores.length; i++) {
 			System.out.print(cloned.scores[i]);
 			System.out.print((i==(cloned.scores.length-1))?"":",");
@@ -27,7 +27,7 @@ public class MemberExample {
 		System.out.println("[원본 객체의 필드값]");
 		System.out.println("name: " + original.name);
 		System.out.println("age: " + original.age);
-		System.out.println("scores: {");
+		System.out.print("scores: {");
 		for(int i = 0; i<original.scores.length; i++) {
 			System.out.print(original.scores[i]);
 			System.out.print((i==(original.scores.length-1))?"":",");
