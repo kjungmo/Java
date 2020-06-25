e835976d3ea9d0028b17d935b7696ace85fc7b4f
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-25 09:56:21 +0900

    Null 여부 조사

---
+++ chap11/src/sec04/exam01_objects/NullExample.java
@@ -0,0 +1,33 @@
+package sec04.exam01_objects;
+
+import java.util.*;
+
+public class NullExample {
+
+	public static void main(String[] args) {
+
+		String str1 = "홍길동";
+		String str2 = null;
+		
+		System.out.println(Objects.requireNonNull(str1));
+		
+		try {
+			String name = Objects.requireNonNull(str2);
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			String name = Objects.requireNonNull(str2, "no name");
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			String name = Objects.requireNonNull(str2, ()->"이름이 없다니깐요");
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
