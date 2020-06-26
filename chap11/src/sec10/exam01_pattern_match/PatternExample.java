888adc1e1a5e6972dc8f37eb0999012f246cb036
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-26 11:31:57 +0900

    문자열 검증하기

---
+++ chap11/src/sec10/exam01_pattern_match/PatternExample.java
@@ -0,0 +1,25 @@
+package sec10.exam01_pattern_match;
+
+import java.util.regex.Pattern;
+
+public class PatternExample {
+	public static void main(String[] args) {
+		String regExp = "(02|010)-\\d{3,4}-\\d{4}"; //자바에서는 역슬래시 하나가 의미하는게 있기 때문에 2개 써준다.
+		String data = "010-123-4567";
+		boolean result = Pattern.matches(regExp, data);
+		if(result) {
+			System.out.println("정규식과 일치합니다.");
+		} else {
+			System.out.println("정규식과 일치하지 않습니다.");
+		}
+		
+		regExp = "\\w+@\\w+\\.\\w+(\\.\\w+)?";
+		data = "angel@navercom";
+		result = Pattern.matches(regExp, data);
+		if(result) {
+			System.out.println("정규식과 일치합니다.");
+		} else {
+			System.out.println("정규식과 일치하지 않습니다.");
+		}
+	}
+}
