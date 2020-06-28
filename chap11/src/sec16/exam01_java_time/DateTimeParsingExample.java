437f0041acc3a3dee1a8eee7ca52d70aec1c95d7
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-28 12:54:13 +0900

    parsing 메소드

--- chap11/src/sec16/exam01_java_time/DateTimeParsingExample.java
+++ chap11/src/sec16/exam01_java_time/DateTimeParsingExample.java
@@ -2,7 +2,8 @@ package sec16.exam01_java_time;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
+		//문자열을 parsing해서 날짜와 시간을 생성하는 메소드
+		// 날짜와 시간 정보가 포함된 문자열을 파싱해서 날짜와 식나을 생성하는 두개의 parse()정적 메소드
 public class DateTimeParsingExample {
 	public static void main(String[] args) {
 		DateTimeFormatter formatter;
