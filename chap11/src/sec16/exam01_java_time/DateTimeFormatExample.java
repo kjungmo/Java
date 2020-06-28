fddbc321e53a7bcc0bf1226cd2ac47a9629cff15
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-28 12:59:22 +0900

    formatting메소드

--- chap11/src/sec16/exam01_java_time/DateTimeFormatExample.java
+++ chap11/src/sec16/exam01_java_time/DateTimeFormatExample.java
@@ -2,7 +2,8 @@ package sec16.exam01_java_time;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+//날짜와 시간을 포맷팅된 문자열로 변환시키는 format()메소드
+//format()의 매개값은 DateTimeFormatter로 해당 형식대로 문자열을 리턴한다.
 public class DateTimeFormatExample {
 	public static void main(String[] args) {
 		LocalDateTime now = LocalDateTime.now();
