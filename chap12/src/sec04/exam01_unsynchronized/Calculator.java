6b1677d71dfee986f02c9deaed4bb1dd38befbdb
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 19:39:57 +0900

    공유객체 사용시 주의 점 calculator공유 객체

--- chap12/src/sec04/exam01_unsynchronized/Calculator.java
+++ chap12/src/sec04/exam01_unsynchronized/Calculator.java
@@ -7,12 +7,12 @@ public class Calculator {
 		return memory;
 	}
 
-	public void setMemory(int memory) {
-		this.memory = memory;
+	public void setMemory(int memory) { //외부에서 값을 받아서 계산기 메모리에 값을 저장하는 메소두ㅡ
+		this.memory = memory; // 매개값을 memory 필드에 저장
 		try {
 			Thread.sleep(2000);
-		} catch(InterruptedException e) {}	
+		} catch(InterruptedException e) {}	//스레드를 2초간 정지시킴 - 이때 user2의 값으로 바뀜
 		System.out.println(Thread.currentThread().getName() + ": " +  this.memory);
-	}
+	}                      	//실행시킨 스레드의 이름과 변경한 메모리의 값. 
 }
 
