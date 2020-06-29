e2479c76ecc570b93b868a7cc61fca16fc3df25b
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 19:25:37 +0900

    Override

--- chap12/src/sec03/exam01_priority/CalcThread.java
+++ chap12/src/sec03/exam01_priority/CalcThread.java
@@ -2,12 +2,13 @@ package sec03.exam01_priority;
 
 public class CalcThread extends Thread {
 	public CalcThread(String name) {
-		setName(name);
+		setName(name); //스레드 이름 변경
 	}
 	
+	@Override // 재정의 해줘야 Thread10에 priority가 MAX 적용됨
 	public void run() {
 		for(int i=0; i<2000000000; i++) {
 		}
-		System.out.println(getName());
-	}
+		System.out.println(getName()); 
+	} // 스레드가 실행할 내용
 }
