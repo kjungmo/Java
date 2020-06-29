14dd1c99e16667aefb509e841d754c32a1e39650
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 22:47:02 +0900

    ThreadA의 스레드

--- chap12/src/sec06/exam02_yield/ThreadA.java
+++ chap12/src/sec06/exam02_yield/ThreadA.java
@@ -5,11 +5,11 @@ public class ThreadA extends Thread {
 	public boolean work = true;
 	@Override
 	public void run() {
-		while(!stop) {
+		while(!stop) { //stop이 true가 되면 while문 종료
 			if(work) {
 				System.out.println("ThreadA 작업 내용");
 			} else {
-				Thread.yield();
+				Thread.yield(); //work가 false가 되면 다른 스레드에게 실행 양보
 			}
 		}
 		System.out.println("ThreadA 종료");
