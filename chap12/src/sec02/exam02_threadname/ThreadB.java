7f088d541c24c1f8118e5a6dfda13bbc9ba27916
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 17:16:07 +0900

    Thread B 클래스

--- chap12/src/sec02/exam02_threadname/ThreadB.java
+++ chap12/src/sec02/exam02_threadname/ThreadB.java
@@ -3,8 +3,8 @@ package sec02.exam02_threadname;
 public class ThreadB extends Thread {	
 	public void run() {		
 		for(int i=0; i<2; i++) {		
-			System.out.println(getName() + "가 출력한 내용");
-		}
+			System.out.println(getName() + "가 출력한 내용");//스레드 이름 얻기
+		}// Thread B 실행 내용
 	}
 }
 
