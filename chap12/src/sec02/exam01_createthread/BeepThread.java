851b3e91d77591622063715b49d74cbf491da062
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 16:37:15 +0900

    Thread 하위 클래스로부터 생성시키는 방법

--- chap12/src/sec02/exam01_createthread/BeepThread.java
+++ chap12/src/sec02/exam01_createthread/BeepThread.java
@@ -2,8 +2,9 @@ package sec02.exam01_createthread;
 
 import java.awt.Toolkit;
 
-public class BeepThread extends Thread {
-	@Override
+public class BeepThread extends Thread { // 작업 스레드가 실행할 작업을 Runnable로 만들지 않고
+	// Thread의 하위 클래스로 작업 스레드를 정의하면서 작업 내용을 포함시킬 수 있다.
+	@Override //Thread 클래스를 상속한 후 run()메소드를 재정의해서 스레드가 실행할 코드를 작성
 	public void run() {		
 		Toolkit toolkit = Toolkit.getDefaultToolkit();	
 		for(int i=0; i<5; i++) {		
