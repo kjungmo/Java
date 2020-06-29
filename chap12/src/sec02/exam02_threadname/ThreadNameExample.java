fb3ebb422bf4577b33056cafef3a5184eb85e67d
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 17:13:39 +0900

    main Thread 이름 출력 및 UserThread 생성 + 시작

--- chap12/src/sec02/exam02_threadname/ThreadNameExample.java
+++ chap12/src/sec02/exam02_threadname/ThreadNameExample.java
@@ -2,15 +2,15 @@ package sec02.exam02_threadname;
 
 public class ThreadNameExample {
 	public static void main(String[] args) {
-		Thread mainThread = Thread.currentThread();
-		System.out.println("프로그램 시작 스레드 이름: " + mainThread.getName());
+		Thread mainThread = Thread.currentThread(); //이 코드를 실행하는 스레드 객체 얻기 
+		System.out.println("프로그램 시작 스레드 이름: " + mainThread.getName());//스레드 이름 얻기
 		
-		ThreadA threadA = new ThreadA();
-		System.out.println("작업 스레드 이름: " + threadA.getName());
-		threadA.start();
+		ThreadA threadA = new ThreadA();//ThreadA 생성
+		System.out.println("작업 스레드 이름: " + threadA.getName());//스레드 이름 얻기
+		threadA.start();//ThreadA 시작
 		
-		ThreadB threadB = new ThreadB();
-		System.out.println("작업 스레드 이름: " + threadB.getName());
-		threadB.start();
-	}
+		ThreadB threadB = new ThreadB();//ThreadB 생성
+		System.out.println("작업 스레드 이름: " + threadB.getName());//스레드 이름 얻기 
+		threadB.start();//ThreadB 시작
+	}//스레드의 start()메소드는 한번 start()호출되고 나면 다시 호출될 수 없고, 스레드 다시 생성해서 start()호출 해야함
 }
