b80be89c13164b6cf31ff2f90498364a10a3962a
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 16:08:32 +0900

    메인 스레드와 작업 스레드가 동시에 실행되는 코드

--- chap12/src/sec02/exam01_createthread/BeepPrintExample2.java
+++ chap12/src/sec02/exam01_createthread/BeepPrintExample2.java
@@ -5,10 +5,10 @@ import java.awt.Toolkit;
 public class BeepPrintExample2 {
 	public static void main(String[] args) {
 		//how1
-		Runnable beepTask = new BeepTask();
-		Thread thread = new Thread(beepTask);
+		Runnable beepTask = new BeepTask();  //작업 객체 생성하고 이것을 매개값으로 해서
+		Thread thread = new Thread(beepTask); // 위의 매개값으로 작업 스레드를 생성한다. 
 		
-		//how2
+		//how2 Runnable 익명 객체 이용
 		/*Thread thread = new Thread(new Runnable() {
 			@Override
 			public void run() {
@@ -20,7 +20,7 @@ public class BeepPrintExample2 {
 			}
 		});*/
 		
-		//how3
+		//how3 람다식 이용
 		/*Thread thread = new Thread(() -> {
 			Toolkit toolkit = Toolkit.getDefaultToolkit();	
 			for(int i=0; i<5; i++) {		
@@ -29,7 +29,9 @@ public class BeepPrintExample2 {
 			}
 		});*/
 		
-		thread.start();
+		thread.start(); // 위 4라인의 작업 스레드의 start()메소드를 호출하면 
+		// 작업스레드에 의해 BeepTask 객체의 run()메소드가 실행되어 비프음이 발생
+		// 동시에 메인 스레드는 아래의 for문을 실행시켜 0.5초 간격으로 문자열을 출력함
 		
 		for(int i=0; i<5; i++) {
 			System.out.println("띵");
