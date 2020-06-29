afb13cbbd6ed5dd6d3f76c1daf1b7b5c023ae964
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 16:46:32 +0900

    메인스레드와 작업스레드 동시에 실행 되는 방식

--- chap12/src/sec02/exam01_createthread/BeepPrintExample3.java
+++ chap12/src/sec02/exam01_createthread/BeepPrintExample3.java
@@ -5,9 +5,9 @@ import java.awt.Toolkit;
 public class BeepPrintExample3 {
 	public static void main(String[] args) {
 		//how1
-		Thread thread = new BeepThread();
+		Thread thread = new BeepThread(); // BeepThread 객체를 생성
 		
-		//how2
+		//how2 
 		/*Thread thread = new Thread() {
 			@Override
 			public void run() {		
@@ -20,9 +20,9 @@ public class BeepPrintExample3 {
 		};*/
 		
 		
-		thread.start();		
+		thread.start();		// start()메소드 호출하여 BeepThread가 run()메소드 실행
 		
-		for(int i=0; i<5; i++) {
+		for(int i=0; i<5; i++) { // 위에서 BeepThread가 run()메소드 실행함과 동시에 for문 실행
 			System.out.println("띵");
 			try { Thread.sleep(500); } catch(Exception e) {}
 		}
