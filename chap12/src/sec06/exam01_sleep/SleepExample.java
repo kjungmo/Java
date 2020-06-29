268a672100e3ce7d5d97fad04217790072d5022c
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 22:17:06 +0900

    sound이외에 출력 문자열을 줌

--- chap12/src/sec06/exam01_sleep/SleepExample.java
+++ chap12/src/sec06/exam01_sleep/SleepExample.java
@@ -7,10 +7,17 @@ public class SleepExample {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();		
 		for(int i=0; i<10; i++) {
 			toolkit.beep();
+			System.out.println("BEEPIN'");
 			try {
 				Thread.sleep(3000); //3초 동안 메인 스레드를 일시 정지 상태로 만듬
-			} catch(InterruptedException e) {			
-			}		
+					for(int i2 = 0; i2<3 ; i2++) {
+						System.out.println("WAITIN'");
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {}
+					}	
+			} 
+			catch(InterruptedException e) {}		
 		}	
 	}
 }
