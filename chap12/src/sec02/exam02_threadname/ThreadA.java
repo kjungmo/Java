883a8fb310f2c9053fe7a5c29411a3614242fc37
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 17:14:20 +0900

    ThreadA 클래스

--- chap12/src/sec02/exam02_threadname/ThreadA.java
+++ chap12/src/sec02/exam02_threadname/ThreadA.java
@@ -2,13 +2,13 @@ package sec02.exam02_threadname;
 
 public class ThreadA extends Thread {	
 	public ThreadA() {
-		setName("ThreadA");
+		setName("ThreadA"); //스레드 이름 설정
 	}
 	
 	public void run() {		
 		for(int i=0; i<2; i++) {		
-			System.out.println(getName() + "가 출력한 내용");
-		}
+			System.out.println(getName() + "가 출력한 내용");//스레드 이름 얻기
+		}//Thread A 실행 내용
 	}
 }
 
