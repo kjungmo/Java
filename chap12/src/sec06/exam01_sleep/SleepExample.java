163fe4ef1fe024a9d49c4ebcfcafc5da7217a2df
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 22:07:30 +0900

    sleep()메소드

--- chap12/src/sec06/exam01_sleep/SleepExample.java
+++ chap12/src/sec06/exam01_sleep/SleepExample.java
@@ -8,7 +8,7 @@ public class SleepExample {
 		for(int i=0; i<10; i++) {
 			toolkit.beep();
 			try {
-				Thread.sleep(3000);
+				Thread.sleep(3000); //3초 동안 메인 스레드를 일시 정지 상태로 만듬
 			} catch(InterruptedException e) {			
 			}		
 		}	
