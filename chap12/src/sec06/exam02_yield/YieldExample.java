d471fdad172e708c9e465df4ea17887fba161665
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 22:45:54 +0900

    스레드 실행 양보 yield()메소드

--- chap12/src/sec06/exam02_yield/YieldExample.java
+++ chap12/src/sec06/exam02_yield/YieldExample.java
@@ -3,18 +3,18 @@ package sec06.exam02_yield;
 public class YieldExample {
 	public static void main(String[] args) {
 		ThreadA threadA = new ThreadA();
-		ThreadB threadB = new ThreadB();	
+		ThreadB threadB = new ThreadB();	//ThreadA, ThreadB 모두 실행
 		threadA.start();		
 		threadB.start();
+		//처음 실행 수 3초 동안 ThreadA와  ThreadB가 번갈아가며 실행됨
+		try { Thread.sleep(3000); } catch (InterruptedException e) {}
+		threadA.work = false; //ThreadB만 실행
 		
 		try { Thread.sleep(3000); } catch (InterruptedException e) {}
-		threadA.work = false;
+		threadA.work = true; // ThreadA, ThreadB 모두 실행
 		
 		try { Thread.sleep(3000); } catch (InterruptedException e) {}
-		threadA.work = true;
-		
-		try { Thread.sleep(3000); } catch (InterruptedException e) {}
-		threadA.stop = true;
+		threadA.stop = true; // ThreadA, ThreadB 모두 종료
 		threadB.stop = true;
 	}
 }
