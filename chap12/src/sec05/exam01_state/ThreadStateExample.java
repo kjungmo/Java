960b08757807653ab9343a36b56eeb712d2c7075
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 21:46:49 +0900

    TargetThread상태 출력 실행 클래스

--- chap12/src/sec05/exam01_state/ThreadStateExample.java
+++ chap12/src/sec05/exam01_state/ThreadStateExample.java
@@ -1,8 +1,9 @@
 package sec05.exam01_state;
-
+//StatePrintThread를 생성해서 
+//매개값으로 전달받은 TargetThread의 상태를 출력하도록 작성된 실행 클래스
 public class ThreadStateExample {
 	public static void main(String[] args) {
 		StatePrintThread statePrintThread = new StatePrintThread(new TargetThread());
-		statePrintThread.start();
+		statePrintThread.start(); 
 	}
 }
