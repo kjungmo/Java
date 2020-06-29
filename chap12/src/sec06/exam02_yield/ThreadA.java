40641f20f42fd043903ecc9acafedaf968f6d275
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-29 22:41:28 +0900

    재정의

--- chap12/src/sec06/exam02_yield/ThreadA.java
+++ chap12/src/sec06/exam02_yield/ThreadA.java
@@ -3,7 +3,7 @@ package sec06.exam02_yield;
 public class ThreadA extends Thread {	
 	public boolean stop = false;
 	public boolean work = true;
-	
+	@Override
 	public void run() {
 		while(!stop) {
 			if(work) {
