9ee77e82abac739b1aef35b5cee4860d76aa8425
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-30 15:48:32 +0900

    스레드 그룹을 이용한 일괄 종료

--- chap12/src/sec08/exam01_threadgroup/ThreadGroupExample.java
+++ chap12/src/sec08/exam01_threadgroup/ThreadGroupExample.java
@@ -5,13 +5,15 @@ public class ThreadGroupExample {
 		ThreadGroup myGroup = new ThreadGroup("myGroup");
 		WorkThread workThreadA = new WorkThread(myGroup, "workThreadA");
 		WorkThread workThreadB = new WorkThread(myGroup, "workThreadB");
+		//myGroup에 두 스레드를 포함시킴
 		
 		workThreadA.start();
 		workThreadB.start();
 		
 		System.out.println("[ main 스레드 그룹의 list() 메소드 출력 내용 ]");
 		ThreadGroup mainGroup = Thread.currentThread().getThreadGroup();		
-		mainGroup.list();
+		mainGroup.list(); //list()메소드는 현재 스레드 그룹의 이름과 최대 우선순위를 헤더로 출력,
+		// 그 아래에 현재 스레드 그룹에 포함된 스레드와 하위 스레드 그룹의 내용을 보여줌.
 		System.out.println();
 		
 		try {
@@ -20,6 +22,6 @@ public class ThreadGroupExample {
 		}
 		
 		System.out.println("[ myGroup 스레드 그룹의 interrupt() 메소드 호출 ]");
-		myGroup.interrupt();
+		myGroup.interrupt(); //스레드 그룹의interrupt()메소드를 호출하면 myGroup에 포함된 두 스레드에서 InterruptException이 발생되어 스레드가 종료됨
 	}
 }
