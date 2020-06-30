1e5ae0069af1031c869a88b51b249c193a67684f
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-30 14:17:57 +0900

    데이터를 생산(저장)하는 스레드

--- chap12/src/sec06/exam05_wait_notify/ProducerThread.java
+++ chap12/src/sec06/exam05_wait_notify/ProducerThread.java
@@ -4,14 +4,14 @@ public class ProducerThread extends Thread {
 	private DataBox dataBox;
 
 	public ProducerThread(DataBox dataBox) {
-		this.dataBox = dataBox;
+		this.dataBox = dataBox;//공유객체를 필드에 저장
 	}
 	
 	@Override
 	public void run() {
 		for(int i=1; i<=3; i++) {
 			String data = "Data-" + i;
-			dataBox.setData(data);
+			dataBox.setData(data); //새로운 데이터를 저장
 		}
 	}
 }
