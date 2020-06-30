9c5fca1f31cd0ec18bb4c3d273fbb9e576aa5771
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-30 15:15:39 +0900

    데몬 스레드 관련 save()메소드 호출하는 데몬스레드

--- chap12/src/sec07/exam01_daemon/AutoSaveThread.java
+++ chap12/src/sec07/exam01_daemon/AutoSaveThread.java
@@ -7,13 +7,13 @@ public class AutoSaveThread extends Thread {
 	
 	@Override
 	public void run() {
-		while(true) {
+		while(true) { //무한 루프
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(1000); // 1초 단위로
 			} catch (InterruptedException e) {
-				break;
+				break; //예외 발생시 while문 종료
 			}
-			save();
+			save(); //save() 하게 되면 맨 위의 작업내용을 저장함이 출력됨
 		}
 	}
 }
