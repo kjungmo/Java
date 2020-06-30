8df0f3c5e3d2230bc1a24fbd212cdca367c151ac
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-30 15:45:28 +0900

    InterruptedException이 발생할 때 스레드가 종료되도록

--- chap12/src/sec08/exam01_threadgroup/WorkThread.java
+++ chap12/src/sec08/exam01_threadgroup/WorkThread.java
@@ -1,18 +1,18 @@
 package sec08.exam01_threadgroup;
 
-public class WorkThread extends Thread {
+public class WorkThread extends Thread { //threadGroup에 포함된다
 	public WorkThread(ThreadGroup threadGroup, String threadName) {
-		super(threadGroup, threadName);
+		super(threadGroup, threadName); //상위 스레드의 생성자를 호출한다. 스레드 그룹과 이름을 설정
 	}
 	
 	@Override
 	public void run() {
-		while(true) {
+		while(true) { //무한 루프
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				System.out.println(getName() + " interrupted");
-				break;
+				break; //InterruptedException이 발생되면 while문을 빠져나와 스레드를 종료시킴
 			}
 		}
 		System.out.println(getName() + " 종료됨");
