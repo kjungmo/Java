40bea49274084576a28e289397db89fd52a08951
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-30 15:10:14 +0900

    무한반복해서 출력하는 스레드 (interrupted() 사용)

--- chap12/src/sec06/exam06_stop/PrintThread2.java
+++ chap12/src/sec06/exam06_stop/PrintThread2.java
@@ -11,12 +11,12 @@ public class PrintThread2 extends Thread {
 		} catch(InterruptedException e) {		
 		}*/
 		
-		//how2
+		//how2 이렇게 쓸 수도 있다. (일시정지를 만들지 않고도 interrupt()호출 여부를 알 수 있는 방법.
 		while(true) {
 			System.out.println("실행 중");
-			if(Thread.interrupted()) {
-				break;
-			}
+			if(Thread.interrupted()) { //interrupt() 메소드가 호출 되었다면 스레드의 interrupted()와 isInterrupted()메소드는 true를 리턴한다.
+				break; //interrupted()는 정적 메소드로 현재 스레드가 interrupted 되었는지 확인하는 것
+			}  //isInterrupted()는 인스턴스 메소드로 현재 스레드가 interrupted 되었는지 확인할 때 사용
 		}
 		
 		System.out.println("자원 정리");
