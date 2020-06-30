10ea2937fab722025ae0650b223a463436f0f136
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-30 13:25:05 +0900

    join()은 다른 스레드가 종료될 때까지 일시정지 상태 유지

--- chap12/src/sec06/exam03_join/JoinExample.java
+++ chap12/src/sec06/exam03_join/JoinExample.java
@@ -5,7 +5,7 @@ public class JoinExample {
 		SumThread sumThread = new SumThread();
 		sumThread.start();
 		try {
-			sumThread.join();
+			sumThread.join(); //sumThread가 종료할 때까지 메인 스레드를 일시 정지시킴
 		} catch (InterruptedException e) {
 		}
 		System.out.println("1~100 합: " + sumThread.getSum());
