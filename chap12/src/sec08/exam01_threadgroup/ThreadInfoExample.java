bbd3c554e6c486ffbc057ae69f8ffc8decc3dba2
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-30 15:25:02 +0900

    현재 실행 중인 스레드 정보

--- chap12/src/sec08/exam01_threadgroup/ThreadInfoExample.java
+++ chap12/src/sec08/exam01_threadgroup/ThreadInfoExample.java
@@ -6,13 +6,13 @@ import java.util.Set;
 public class ThreadInfoExample {
 	public static void main(String[] args) {
 		AutoSaveThread autoSaveThread = new AutoSaveThread();
-		autoSaveThread.setName("AutoSaveThread");
+		autoSaveThread.setName("AutoSaveThread");//이렇게 이름을 안주면 "Thread-0"이런식으로 자동 부여됨
 		autoSaveThread.setDaemon(true);
 		autoSaveThread.start();
 		
 		Map<Thread, StackTraceElement[]> map = Thread.getAllStackTraces();
-		Set<Thread> threads = map.keySet();
-		for(Thread thread : threads) {
+		Set<Thread> threads = map.keySet(); // Process에서 실행하는 모든 Thread를 가져오는 코드
+		for(Thread thread : threads) { //Thread를 하나씩 가져와 루핑
 			System.out.println("Name: " + thread.getName() + ((thread.isDaemon())?"(데몬)": "(주)"));
 			System.out.println("\t" + "소속그룹: " + thread.getThreadGroup().getName());
 			System.out.println();
