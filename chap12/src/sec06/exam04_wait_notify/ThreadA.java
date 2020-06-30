f3b00767185738f9e9bf953a5bcf9b561631650e
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-30 14:10:47 +0900

    workObject의 methodA/B()를 실행하는 스레드

--- chap12/src/sec06/exam04_wait_notify/ThreadA.java
+++ chap12/src/sec06/exam04_wait_notify/ThreadA.java
@@ -1,14 +1,14 @@
 package sec06.exam04_wait_notify;
 
 public class ThreadA extends Thread {
-	private WorkObject workObject;
+	private WorkObject workObject; //공유객체를 매개값으로 받아 필드에 저장
 
 	public ThreadA(WorkObject workObject) {
 		this.workObject = workObject;
 	}
 	
 	@Override
-	public void run() {
+	public void run() { //공유객체의 methodA()를 10번 반복 호출
 		for(int i=0; i<10; i++) {
 			workObject.methodA();
 		}
--- chap12/src/sec06/exam04_wait_notify/ThreadB.java
+++ chap12/src/sec06/exam04_wait_notify/ThreadB.java
@@ -4,11 +4,11 @@ public class ThreadB extends Thread {
 	private WorkObject workObject;
 
 	public ThreadB(WorkObject workObject) {
-		this.workObject = workObject;
+		this.workObject = workObject; //공유객체를 매개값으로 받아 필드에 저장
 	}
 	
 	@Override
-	public void run() {
+	public void run() { //공유객체의 methodB()를 10번 반복 호출
 		for(int i=0; i<10; i++) {
 			workObject.methodB();
 		}
