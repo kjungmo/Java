41b4f24034ee3c77a7dfa8893145c4c0d87c74e8
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-30 14:31:50 +0900

    스레드 생성, 실행하는 메인 스레드

--- chap12/src/sec06/exam05_wait_notify/WaitNotifyExample.java
+++ chap12/src/sec06/exam05_wait_notify/WaitNotifyExample.java
@@ -4,9 +4,11 @@ public class WaitNotifyExample {
 	public static void main(String[] args) {
 		DataBox dataBox = new DataBox();
 		
+		// 스레드를 생성시켜줌
 		ProducerThread producerThread = new ProducerThread(dataBox);
 		ConsumerThread consumerThread = new ConsumerThread(dataBox);
 		
+		//start() 
 		producerThread.start();
 		consumerThread.start();
 	}
