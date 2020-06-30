cba5f507eda236622babfa541926c25e0b0effe3
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-30 14:17:14 +0900

    두 스레드의 작업 내용을 동기화 메소드로 작성한 공유 객체

--- chap12/src/sec06/exam05_wait_notify/DataBox.java
+++ chap12/src/sec06/exam05_wait_notify/DataBox.java
@@ -3,28 +3,28 @@ package sec06.exam05_wait_notify;
 public class DataBox {
 	private String data;
 	
-	public synchronized String getData() {
-		if(this.data == null) {
+	public synchronized String getData() { //생산자 스레드가 뭔가를 저장해둔 상태라면
+		if(this.data == null) { //data필드가 null이면 소비자 스레드를 일시정지상태로
 			try {
 				wait();
 			} catch(InterruptedException e) {}
 		}
 		String returnValue = data;
 		System.out.println("ConsummerThread가 읽은 데이터: " + returnValue);
-		data = null;
+		data = null; // data필드를 null로 만들고 생산자 스레드를 실행대기 상태로 (읽었으니까 다시 null로)
 		notify();
 		return returnValue;
 	}
 	
 	public synchronized void setData(String data) {
 		if(this.data != null) {
-			try {
+			try {		//data 필드가 null이 아니면 생산자 스레드를 일시정지상태로
 				wait();
 			} catch(InterruptedException e) {}
 		}
 		this.data = data;
 		System.out.println("ProducerThread가 생성한 데이터: " + data);
-		notify();
+		notify(); //data 필드에 값을 저장하고 소비자 스레드를 실행대기 상태로 
 	}
 }
 
